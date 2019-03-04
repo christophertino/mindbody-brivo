2ed00b838bb4dec9fd5fd9b1bb67427ade29a1e1
Christopher Tino <christopher_tino@yahoo.com>
2019-03-04 17:49:52 -0500

    fetch Brivo access token

--- models/config.go
+++ models/config.go
@@ -12,14 +12,14 @@ package models
 
 // Config : Settings imported from conf.json
 type Config struct {
+	BrivoUsername     string `json:"brivo_username"`
+	BrivoPassword     string `json:"brivo_password"`
 	BrivoClientID     string `json:"brivo_client_id"`
 	BrivoClientSecret string `json:"brivo_client_secret"`
 	BrivoAPIKey       string `json:"brivo_api_key"`
 
-	MindbodySourceName string `json:"mindbody_source_name"`
-	MindbodySourcePass string `json:"mindbody_source_pass"`
-	MindbodyAPIKey     string `json:"mindbody_api_key"`
-	MindbodyUsername   string `json:"mindbody_username"`
-	MindbodyPassword   string `json:"mindbody_password"`
-	MindbodySite       string `json:"mindbody_site"`
+	MindbodyAPIKey   string `json:"mindbody_api_key"`
+	MindbodyUsername string `json:"mindbody_username"`
+	MindbodyPassword string `json:"mindbody_password"`
+	MindbodySite     string `json:"mindbody_site"`
 }
