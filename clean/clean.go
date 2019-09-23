f10df67e048d72fe2ee6e7c9045a013803762a85
Christopher Tino <christopher_tino@yahoo.com>
2019-09-22 21:26:08 -0400

    add error channel to Clean app to handle token refreshing

--- clean/clean.go
+++ clean/clean.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/beefsack/go-rate"
+	utils "github.com/christophertino/mindbody-brivo"
 	"github.com/christophertino/mindbody-brivo/models"
 )
 
@@ -26,16 +27,21 @@ var (
 	brivo        models.Brivo
 	creds        models.CredentialList
 	config       *models.Config
-	customFields models.CustomFields
 	rateLimit    *rate.RateLimiter
 	wg           sync.WaitGroup
 	brivoIDs     brivoIDSet
+	isRefreshing bool
+	errUser      chan *models.BrivoUser
+	errCred      chan *models.Credential
 )
 
 // Nuke is meant for cleaning up your Brivo developer environment. It will
 // remove all existing users and credentials so that you can start fresh.
 func Nuke(cfg *models.Config) {
 	config = cfg
+	isRefreshing = false
+	errUser = make(chan *models.BrivoUser, config.BrivoRateLimit)
+	errCred = make(chan *models.Credential, config.BrivoRateLimit)
 
 	// Generate Brivo access token
 	if err := auth.BrivoToken.GetBrivoToken(config); err != nil {
@@ -65,75 +71,168 @@ func Nuke(cfg *models.Config) {
 
 	// Loop over all users and delete
 	for _, user := range brivo.Data {
-		wg.Add(1)
-		go func(u models.BrivoUser) {
-			defer wg.Done()
-
-			// Check for valid Brivo AccessToken
-			if time.Now().UTC().After(auth.BrivoToken.ExpireTime) {
-				rateLimit.Wait()
-				fmt.Println("Refresh required...")
-				if err := auth.BrivoToken.RefreshBrivoToken(*config); err != nil {
-					log.Fatalln("Error refreshing Brivo AUTH token", err)
-				}
-				fmt.Println("Refreshed Brivo AUTH token")
-			}
-
-			// Get custom fields for user
-			rateLimit.Wait()
-			if err := customFields.GetCustomFieldsForUser(u.ID, config.BrivoAPIKey, auth.BrivoToken.AccessToken); err != nil {
-				fmt.Printf("Error fetching custom fields for user %d: %s\n", u.ID, err)
-				return
-			}
-
-			// Stash Barcode ID in a set so we can check it later against Credential.ReferenceID
-			barcodeID, err := models.GetFieldValue(config.BrivoBarcodeFieldID, customFields.Data)
-			if err != nil {
-				fmt.Printf("Skipping user %d with error: %s\n", u.ID, err)
-				return
-			}
-			brivoIDs.update(barcodeID)
-
-			// Delete the user
-			rateLimit.Wait()
-			if err := u.DeleteUser(config.BrivoAPIKey, auth.BrivoToken.AccessToken); err != nil {
-				fmt.Printf("Error deleting user %d: %s", u.ID, err)
-			}
-		}(user)
+		if isRefreshing {
+			errUser <- &user
+			continue
+		}
+		processUser(user)
 	}
+
+	wg.Wait()
 
 	fmt.Println("Deleteing all Brivo credentials...")
 
 	// Loop over all credentials and delete
 	for _, cred := range creds.Data {
-		wg.Add(1)
-		go func(c models.Credential) {
-			defer wg.Done()
-
-			// Check for valid Brivo AccessToken
-			if time.Now().UTC().After(auth.BrivoToken.ExpireTime) {
-				rateLimit.Wait()
-				fmt.Println("Refresh required...")
-				if err := auth.BrivoToken.RefreshBrivoToken(*config); err != nil {
-					log.Fatalln("Error refreshing Brivo AUTH token", err)
-				}
-				fmt.Println("Refreshed Brivo AUTH token")
-			}
-
-			// Make sure this credential belongs to a user we are deleteing (in the Member group only)
-			if brivoIDs.ids[c.ReferenceID] == true {
-				rateLimit.Wait()
-				if err := c.DeleteCredential(config.BrivoAPIKey, auth.BrivoToken.AccessToken); err != nil {
-					fmt.Printf("Error deleting credential %d: %s\n", c.ID, err)
-				}
-			}
-		}(cred)
+		if isRefreshing {
+			errCred <- &cred
+			continue
+		}
+		processCredential(cred)
 	}
 
 	// Wait for all routines to finish and close
 	wg.Wait()
 
 	fmt.Println("Nuke completed. Check error logs for output.")
+}
+
+// Fetch the user's Barcode ID and delete the user from Brivo
+func processUser(user models.BrivoUser) {
+	wg.Add(1)
+	go func(u models.BrivoUser) {
+		defer wg.Done()
+
+		// Get custom fields for user
+		customFields, err := getCustomFields(&u)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Stash Barcode ID in a set so we can check it later against Credential.ReferenceID
+		barcodeID, err := models.GetFieldValue(config.BrivoBarcodeFieldID, customFields.Data)
+		if err != nil {
+			fmt.Printf("Skipping user %d with error: %s\n", u.ID, err)
+			return
+		}
+		brivoIDs.update(barcodeID)
+
+		// Delete the user
+		if err := deleteUser(&u); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("Deleted user %d\n", u.ID)
+	}(user)
+}
+
+// Remove the credential from Brivo concurrently
+func processCredential(cred models.Credential) {
+	wg.Add(1)
+	go func(c models.Credential) {
+		defer wg.Done()
+
+		// Make sure this credential belongs to a user we are deleteing (in the Member group only)
+		if brivoIDs.ids[c.ReferenceID] == true {
+			// Delete the credential
+			if err := deleteCredential(&c); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+		fmt.Printf("Deleted credential %d\n", c.ID)
+	}(cred)
+}
+
+// Get user's custom fields
+func getCustomFields(user *models.BrivoUser) (models.CustomFields, error) {
+	rateLimit.Wait()
+	var customFields models.CustomFields
+	err := customFields.GetCustomFieldsForUser(user.ID, config.BrivoAPIKey, auth.BrivoToken.AccessToken)
+	switch e := err.(type) {
+	case nil:
+		return customFields, nil
+	case *utils.JSONError:
+		if e.Code == 401 {
+			errUser <- user
+			doRefresh()
+			return customFields, fmt.Errorf("Access token expired")
+		}
+	}
+	return customFields, fmt.Errorf("Error fetching custom fields for user %d: %s", user.ID, err)
+}
+
+// Delete a user from Brivo
+func deleteUser(user *models.BrivoUser) error {
+	rateLimit.Wait()
+	err := user.DeleteUser(config.BrivoAPIKey, auth.BrivoToken.AccessToken)
+	switch e := err.(type) {
+	case nil:
+		return nil
+	case *utils.JSONError:
+		if e.Code == 401 {
+			errUser <- user
+			doRefresh()
+			return fmt.Errorf("Access token expired")
+		}
+	}
+	return fmt.Errorf("Error deleting user %d: %s", user.ID, err)
+}
+
+// Delete a credential from Brivo
+func deleteCredential(cred *models.Credential) error {
+	rateLimit.Wait()
+	err := cred.DeleteCredential(config.BrivoAPIKey, auth.BrivoToken.AccessToken)
+	switch e := err.(type) {
+	case nil:
+		return nil
+	case *utils.JSONError:
+		if e.Code == 401 {
+			errCred <- cred
+			doRefresh()
+			return fmt.Errorf("Access token expired")
+		}
+	}
+	return fmt.Errorf("Error deleting credential %d: %s", cred.ID, err)
+}
+
+// Call Brivo and fetch a refreshed token
+func refreshToken() error {
+	rateLimit.Wait()
+	if err := auth.BrivoToken.RefreshBrivoToken(*config); err != nil {
+		return fmt.Errorf("Error refreshing Brivo token: %s", err)
+	}
+	return nil
+}
+
+// Check current refreshing status and process new refresh token
+func doRefresh() {
+	if isRefreshing {
+		return
+	}
+
+	isRefreshing = true
+	if err := refreshToken(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Refreshed Brivo AUTH token")
+	isRefreshing = false
+
+	// Listen for new events in the error channels
+loop:
+	for {
+		select {
+		case user := <-errUser:
+			processUser(*user)
+		case cred := <-errCred:
+			processCredential(*cred)
+		default:
+			break loop
+		}
+	}
 }
 
 // Uses mutual exclusion for thread-safe update to brivoIDSet map[]
